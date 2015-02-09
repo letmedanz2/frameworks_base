86fdf0a0db49d8b545b6b63080796dcbf03ce1e3
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:55:29 +0000

    QS: add Expanded desktop tile
    Change-Id: Icb4ef73eea923acf9cde52f6c66750a26360f238

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -43,6 +43,7 @@ public class QSConstants {
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_BRIGHTNESS = "brightness";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
+    public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
 
     public static final String TILE_REBOOT = "reboot";
 
@@ -77,5 +78,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_REBOOT);
         TILES_AVAILABLE.add(TILE_BRIGHTNESS);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
+        TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
     }
 }
