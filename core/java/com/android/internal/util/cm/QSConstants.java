82f69f5f70dffb05dd9c97fcd617353dd5a066d7
letmedanz <dan.hennighan@yahoo.com>
2015-02-24 03:27:18 +0530

    QSTile: Reboot/Recovery tile (1/2)
    Forward port from SlimKat
    
    Change-Id: Ia933ffd48c485a8a6ca72a3c60139b144ac80e90
    
    Conflicts:
    	packages/SystemUI/res/values/slim_strings.xml

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -38,6 +38,7 @@ public class QSConstants {
     public static final String TILE_LTE = "lte";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_VISUALIZER = "visualizer";
+    public static final String TILE_REBOOT = "reboot";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -66,5 +67,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LTE);
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_VISUALIZER);
+        TILES_AVAILABLE.add(TILE_REBOOT);
     }
 }
