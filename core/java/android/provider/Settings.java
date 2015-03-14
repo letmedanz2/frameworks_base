7f9e66af1923b3638e7238cfb75152f1b76bc610
letmedanz <dan.hennighan@yahoo.com>
2015-03-14 20:41:01 +0530

    XuiMod: Toast Animations [1/2]
    Add Xylon, Xperia Z1, TokoROM Transition and more
    
    https://github.com/zst123/XuiMod/commit/8fc1152fb485ed5e007ac8d4ea9ad7b7f1c493d8
    
    https://github.com/zst123/XuiMod/commit/1989a90850e5491799620e1f86fcbc9ae54f5161
    
    https://github.com/zst123/XuiMod/commit/e3383f127244822b9fc61db169b59e4a502d127b
    
    Credit zst123, thanks for GwonHyeok
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ic9ec14ac0e8288a14c0ff46b70dff34e7505a719

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3403,6 +3403,11 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
+         * Toast Animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
